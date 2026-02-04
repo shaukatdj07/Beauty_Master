0978931c5b8ba3d58837e15df0e17f625009be75
Shaukat Ullah <shaukatdarshi777@gmail.com>
2026-02-04 23:33:45 +0500

    Start Working on beauty app project, which is almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,45 @@
+//region imports
 import 'package:flutter/material.dart';
-import 'package:uis/home_screen.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'beauty_app/screens/BMSplashScreen.dart';
+import 'beauty_app/store/AppStore.dart';
+import 'beauty_app/utils/AppTheme.dart';
+import 'beauty_app/utils/BMConstants.dart';
+import 'beauty_app/utils/BMDataGenerator.dart';
 
-void main() {
-  runApp(const MyApp());
+AppStore appStore = AppStore();
+
+int currentIndex = 0;
+
+void main() async {
+  //region Entry Point
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialize(aLocaleLanguageList: languageList());
+
+  appStore.toggleDarkMode(value: getBoolAsync(isDarkModeOnPref));
+
+  defaultRadius = 10;
+  defaultToastGravityGlobal = ToastGravity.BOTTOM;
+
+  runApp(MyApp());
+  //endregion
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return Observer(
+      builder: (_) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '$appName${!isMobile ? ' ${platformName()}' : ''}',
+        home: BMSplashScreen(),
+        theme: !appStore.isDarkModeOn ? AppThemeData.lightTheme : AppThemeData.darkTheme,
+        navigatorKey: navigatorKey,
+        scrollBehavior: SBehavior(),
+        supportedLocales: LanguageDataModel.languageLocales(),
+        localeResolutionCallback: (locale, supportedLocales) => locale,
       ),
-      home: const HomeScreen(),
     );
   }
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import nb_utils
+import package_info
+import path_provider_foundation
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
